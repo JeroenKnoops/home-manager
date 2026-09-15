9cf53b00cb713f3ba500b05b2bc44b1495fa4c49
Jeroen Knoops <jeroen.knoops@philips.com>
2026-09-15 09:56:46 +0200

    cert: remove custom CA umbrella
    Signed-off-by: Jeroen Knoops <jeroen.knoops@philips.com>

--- modules/machines/MACHXPVL4MXK7.nix
+++ modules/machines/MACHXPVL4MXK7.nix
@@ -19,8 +19,6 @@ let
   tag-prs = builtins.getFlake "git+ssh://git@github.com/philips-internal/synergy-build-analyse?dir=tag-prs&rev=94b562e526c8400cb2680e926658e85e18f26ff4";
   pn-buildlist-tool = builtins.getFlake "git+ssh://git@github.com/philips-internal/synergy-build-analyse?dir=pn-buildlist-tool&rev=29f721b3602e0a51788ff6c7bfd258fe7c22e910";
   sslConfigDir = "${config.home.homeDirectory}/.config/ssl";
-  umbrellaCertPath = "${sslConfigDir}/cisco-umbrella.pem";
-  combinedCaBundlePath = "${sslConfigDir}/ca-bundle-with-cisco-umbrella.pem";
 in
 {
   imports = [
@@ -147,6 +145,8 @@ in
     alt-shift-1..5   move window to workspace
     alt-m            fullscreen
     alt-shift-;      service mode
+    alt-slash        layout tiles horizontal vertical 
+    alt-comma        layout accordion horizontal vertical
   '';
 
   home.file.".config/borders/bordersrc" = {
@@ -157,10 +157,6 @@ in
       echo "borders started or updated"
     '';
   };
-
-  home.file.".wgetrc".text = ''
-    ca_certificate = ${combinedCaBundlePath}
-  '';
 
   home.packages = with pkgs; [
     aerospace
@@ -238,39 +234,7 @@ in
 
   home.sessionVariables = {
     DIRENV_LOG_FORMAT = "";
-    CURL_CA_BUNDLE = combinedCaBundlePath;
-    GIT_SSL_CAINFO = combinedCaBundlePath;
-    NIX_SSL_CERT_FILE = combinedCaBundlePath;
-    SSL_CERT_FILE = combinedCaBundlePath;
   };
-
-  # Corporate TLS inspection on this machine re-signs public certificates with
-  # Cisco Umbrella. Export the trusted Umbrella certs from the macOS keychain at
-  # activation time and append them to Nix's default CA bundle so wget, curl,
-  # git, and other Nix-installed CLI tools trust the intercepted chain.
-  home.activation.installCiscoUmbrellaCaBundle = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    sslDir="${sslConfigDir}"
-    umbrellaCert="${umbrellaCertPath}"
-    caBundle="${combinedCaBundlePath}"
-
-    run mkdir -p "$sslDir"
-
-    if [ -n "$DRY_RUN_CMD" ]; then
-      echo "/usr/bin/security find-certificate -a -c Cisco Umbrella -p > $umbrellaCert"
-    else
-      /usr/bin/security find-certificate -a -c "Cisco Umbrella" -p > "$umbrellaCert"
-
-      if [ ! -s "$umbrellaCert" ]; then
-        echo "MACHXPVL4MXK7: failed to export Cisco Umbrella certificates from the macOS keychain; TLS bundle not updated." >&2
-        exit 1
-      fi
-    fi
-
-    $DRY_RUN_CMD /bin/sh -c '${pkgs.coreutils}/bin/cat "$1" "$2" > "$3"' -- \
-      "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" \
-      "$umbrellaCert" \
-      "$caBundle"
-  '';
 
   home.activation.installHerdrIntegrations = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     run mkdir -p "$HOME/.config/opencode" "$HOME/.hermes"
